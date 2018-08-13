e334b2f57a6f720e689fe9a553ff44d224773fd7
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-08-13 00:32:56 -0700

    created dev branch. added file structure and connections code

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name)
+VALUEs ('Big Boy'), ('Baconlicious'), ('Veggie Madness');
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Triple Decker Madness', true), ('Mushroom Swiss King', true),
+("You'll Regret Me in the Morning!", true);
